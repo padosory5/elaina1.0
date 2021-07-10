156fb6a5b06287db24e3a43a75043ddd2f213625
padosory5 <padosory5@gmail.com>
2021-07-10 20:54:31 +0900

    added voice_recognition with time

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ import tflearn
 import numpy
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
+import speech_recognition as sr
+from time import ctime
+import webbrowser
+
 stemmer = LancasterStemmer()
+
+r = sr.Recognizer()
 
 
 with open("intents.json") as file:
     data = json.load(file)
-
+'''
 try:
     with open("data.pickle", "rb") as f:
         words, labels, training, output = pickle.load(f)
@@ -63,6 +69,54 @@ except:
 
     with open("data.pickle", "wb") as f:
         pickle.dump((words, labels, training, output), f)
+'''
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = numpy.array(output)
+
+with open("data.pickle", "wb") as f:
+    pickle.dump((words, labels, training, output), f)
 
 
 tf.compat.v1.reset_default_graph()
@@ -74,12 +128,15 @@ net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
-
+'''
 try:
     model.load("model.tflearn")
 except:
     model.fit(training, output, n_epoch=1500, batch_size=8, show_metric=True)
     model.save("model.tflearn")
+'''
+model.fit(training, output, n_epoch=1500, batch_size=8, show_metric=True)
+model.save("model.tflearn")
 
 
 def bag_of_words(s, words):
@@ -96,11 +153,14 @@ def bag_of_words(s, words):
 
 
 def chat():
-    print("Start talking with Elaina!")
+    print("Start talking with Elaina! (use microphone to talk)")
     while True:
-        inp = input("> ")
 
-        results = model.predict([bag_of_words(inp, words)])
+        voice_data = record_audio()
+
+        print(">", voice_data)
+
+        results = model.predict([bag_of_words(voice_data, words)])
         results_index = numpy.argmax(results)
         tag = labels[results_index]
 
@@ -111,8 +171,22 @@ def chat():
         if tag == 'goodbye':
             print(random.choice(responses))
             break
+        elif tag == 'time':
+            print("it is currently ", ctime())
         else:
             print(random.choice(responses))
+
+
+def record_audio():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print("Sorry, I did not get that")
+        except sr.RequestError:
+            print("Sorry, my speech service is down")
+        return voice_data
 
 
 chat()
