b2ce14b14bd558c3e0a47b94c9aa7d55f5aafc4e
padosory5 <padosory5@gmail.com>
2021-07-10 21:02:09 +0900

    added voice recognition and time

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ r = sr.Recognizer()
 
 with open("intents.json") as file:
     data = json.load(file)
-'''
 try:
     with open("data.pickle", "rb") as f:
         words, labels, training, output = pickle.load(f)
@@ -69,54 +68,6 @@ except:
 
     with open("data.pickle", "wb") as f:
         pickle.dump((words, labels, training, output), f)
-'''
-words = []
-labels = []
-docs_x = []
-docs_y = []
-
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
-
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
-
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-words = sorted(list(set(words)))
-
-labels = sorted(labels)
-
-training = []
-output = []
-
-out_empty = [0 for _ in range(len(labels))]
-
-for x, doc in enumerate(docs_x):
-    bag = []
-
-    wrds = [stemmer.stem(w.lower()) for w in doc]
-
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
-
-    training.append(bag)
-    output.append(output_row)
-
-training = numpy.array(training)
-output = numpy.array(output)
-
-with open("data.pickle", "wb") as f:
-    pickle.dump((words, labels, training, output), f)
 
 
 tf.compat.v1.reset_default_graph()
@@ -128,15 +79,12 @@ net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
-'''
+
 try:
     model.load("model.tflearn")
 except:
     model.fit(training, output, n_epoch=1500, batch_size=8, show_metric=True)
     model.save("model.tflearn")
-'''
-model.fit(training, output, n_epoch=1500, batch_size=8, show_metric=True)
-model.save("model.tflearn")
 
 
 def bag_of_words(s, words):
